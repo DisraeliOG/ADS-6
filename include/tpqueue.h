4601315c7bbd68eacd4e25c5892d133ebaf87300
DisraeliOG <118463303+DisraeliOG@users.noreply.github.com>
2025-05-02 20:58:06 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,87 @@
 // Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TPQUEUE_H_
-#define INCLUDE_TPQUEUE_H_
-
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
+#ifndef TPQUEUE_H
+#define TPQUEUE_H
+#include <cstddef>
+#include <stdexcept>
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
+};
+
+template <typename T>
+class TPQueue {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& d) : data(d), next(nullptr) {}
+    };
+
+    Node* head;
+    std::size_t count;
+
+public:
+    TPQueue() : head(nullptr), count(0) {}
+
+    ~TPQueue() {
+        clear();
+    }
+
+    TPQueue(const TPQueue&) = delete;
+    TPQueue& operator=(const TPQueue&) = delete;
+
+    bool empty() const {
+        return head == nullptr;
+    }
+
+    std::size_t size() const {
+        return count;
+    }
+
+    void push(const T& value) {
+        Node* node = new Node(value);
+        if (!head || value.prior > head->data.prior) {
+            node->next = head;
+            head = node;
+        } else {
+            Node* current = head;
+            while (current->next && current->next->data.prior >= value.prior) {
+                current = current->next;
+            }
+            node->next = current->next;
+            current->next = node;
+        }
+        ++count;
+    }
+
+    T pop() {
+        if (empty()) {
+            throw std::out_of_range("Pop from empty TPQueue");
+        }
+        Node* node = head;
+        T value = node->data;
+        head = head->next;
+        delete node;
+        --count;
+        return value;
+    }
+
+    const T& top() const {
+        if (empty()) {
+            throw std::out_of_range("Top from empty TPQueue");
+        }
+        return head->data;
+    }
+
+    void clear() {
+        while (head) {
+            Node* tmp = head;
+            head = head->next;
+            delete tmp;
+        }
+        count = 0;
+    }
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
